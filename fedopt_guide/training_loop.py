95c36d9d072f61da16f395a1c2b843bd3aed2582
Zachary Charles <zachcharles@google.com>
2020-12-02 13:14:42 -0800

    Create a training loop for fedopt_guide experiments.
    PiperOrigin-RevId: 345297823
    Change-Id: Iba947fd3c6f80e522b4c79d7e780181172ffde4e

---
+++ fedopt_guide/training_loop.py
@@ -0,0 +1,277 @@
+# Copyright 2020, Google LLC.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Training loop for fedopt-guide experiments."""
+
+import os.path
+import pprint
+import time
+from typing import Any, Callable, Dict, List, Optional
+
+from absl import logging
+import pandas as pd
+import tensorflow as tf
+import tensorflow_federated as tff
+
+from utils import checkpoint_manager
+from utils import metrics_manager
+from utils import utils_impl
+from tensorboard.plugins.hparams import api as hp
+
+
+class IterativeProcessCompatibilityError(TypeError):
+  pass
+
+
+def create_if_not_exists(path):
+  try:
+    tf.io.gfile.makedirs(path)
+  except tf.errors.OpError:
+    logging.info('Skipping creation of directory [%s], already exists', path)
+
+
+def _setup_outputs(root_output_dir, experiment_name, hparam_dict):
+  """Set up directories for experiment loops, write hyperparameters to disk."""
+
+  if not experiment_name:
+    raise ValueError('experiment_name must be specified.')
+
+  create_if_not_exists(root_output_dir)
+
+  checkpoint_dir = os.path.join(root_output_dir, 'checkpoints', experiment_name)
+  create_if_not_exists(checkpoint_dir)
+  checkpoint_mngr = checkpoint_manager.FileCheckpointManager(checkpoint_dir)
+
+  results_dir = os.path.join(root_output_dir, 'results', experiment_name)
+  create_if_not_exists(results_dir)
+  metrics_mngr = metrics_manager.ScalarMetricsManager(results_dir)
+
+  summary_logdir = os.path.join(root_output_dir, 'logdir', experiment_name)
+  create_if_not_exists(summary_logdir)
+  summary_writer = tf.summary.create_file_writer(summary_logdir)
+
+  if hparam_dict:
+    hparam_dict['metrics_file'] = metrics_mngr.metrics_filename
+    hparams_file = os.path.join(results_dir, 'hparams.csv')
+    utils_impl.atomic_write_to_csv(pd.Series(hparam_dict), hparams_file)
+    with summary_writer.as_default():
+      hp.hparams({k: v for k, v in hparam_dict.items() if v is not None})
+
+  logging.info('Writing...')
+  logging.info('    checkpoints to: %s', checkpoint_dir)
+  logging.info('    metrics csv to: %s', metrics_mngr.metrics_filename)
+  logging.info('    summaries to: %s', summary_logdir)
+
+  return checkpoint_mngr, metrics_mngr, summary_writer
+
+
+def _write_metrics(metrics_mngr, summary_writer, metrics, round_num):
+  """Atomic metrics writer which inlines logic from MetricsHook class."""
+  if not isinstance(metrics, dict):
+    raise TypeError('metrics should be type `dict`.')
+  if not isinstance(round_num, int):
+    raise TypeError('round_num should be type `int`.')
+
+  flat_metrics = metrics_mngr.update_metrics(round_num, metrics)
+  logging.info('Evaluation at round {:d}:\n{!s}'.format(
+      round_num, pprint.pformat(flat_metrics)))
+
+  # Also write metrics to a tf.summary logdir
+  with summary_writer.as_default():
+    for name, val in flat_metrics.items():
+      tf.summary.scalar(name, val, step=round_num)
+
+
+def _compute_numpy_l2_difference(model, previous_model):
+  squared_norms = tf.nest.map_structure(lambda x, y: tf.linalg.norm(x - y)**2,
+                                        model, previous_model)
+  l2_total_tensor = tf.reduce_sum(tf.nest.flatten(squared_norms))**0.5
+  return l2_total_tensor.numpy()
+
+
+def _check_iterative_process_compatibility(iterative_process):
+  """Checks the compatibility of an iterative process with the training loop."""
+  error_message = (
+      'The iterative_process argument must be of '
+      'type`tff.templates.IterativeProcess`, and must have an '
+      'attribute `get_model_weights`, which must be a `tff.Computation`. This '
+      'computation must accept as input the state of `iterative_process`, and '
+      'its output must be a nested structure of tensors matching the expected '
+      'shape of the first input argument to `evaluation_fn`.')
+  compatibility_error = IterativeProcessCompatibilityError(error_message)
+
+  if not isinstance(iterative_process, tff.templates.IterativeProcess):
+    raise compatibility_error
+  if not hasattr(iterative_process, 'get_model_weights'):
+    raise compatibility_error
+  elif not callable(iterative_process.get_model_weights):
+    raise compatibility_error
+  get_model_weights_fn = iterative_process.get_model_weights
+
+  if not isinstance(get_model_weights_fn, tff.Computation):
+    raise compatibility_error
+  input_type = get_model_weights_fn.type_signature.parameter
+  server_state_type = iterative_process.state_type.member
+  server_state_type.is_assignable_from(input_type)
+  # TODO(b/174268978): Once we enforce federated evaluations, we can check
+  # compatibility with `validation_fn` without actually running the function.
+
+
+def run(iterative_process: tff.templates.IterativeProcess,
+        train_client_datasets_fn: Callable[[int], List[tf.data.Dataset]],
+        eval_client_datasets_fn: Callable[[int], List[tf.data.Dataset]],
+        evaluation_fn: Callable[[Any, List[tf.data.Dataset]], Dict[str, float]],
+        total_rounds: int,
+        experiment_name: str,
+        test_fn: Optional[Callable[[Any], Dict[str, float]]] = None,
+        root_output_dir: Optional[str] = '/tmp/fedopt_guide',
+        hparam_dict: Optional[Dict[str, Any]] = None,
+        rounds_per_eval: Optional[int] = 10,
+        rounds_per_checkpoint: Optional[int] = 50):
+  """Runs federated training for a given `tff.templates.IterativeProcess`.
+
+  We assume that the iterative process has the following functional type
+  signatures:
+
+    *   `initialize`: `( -> S@SERVER)` where `S` represents the server state.
+    *   `next`: `<S@SERVER, {B*}@CLIENTS> -> <S@SERVER, T@SERVER>` where `S`
+        represents the server state, `{B*}` represents the client datasets,
+        and `T` represents a python `Mapping` object.
+
+  The iterative process must also have a callable attribute `get_model_weights`
+  that takes as input the state of the iterative process, and returns a
+  nested structure of tensors that can be input to the `evaluation_fn` and
+  `test_fn` (if provided).
+
+  Args:
+    iterative_process: A `tff.templates.IterativeProcess` instance to run.
+    train_client_datasets_fn: Function accepting an integer argument (the round
+      number) and returning a list of train client datasets to use as federated
+      data for that training round.
+    eval_client_datasets_fn: Function accepting an integer argument (the round
+      number) and returning a list of evaluation client datasets to use as
+      federated data for that evaluation round.
+    evaluation_fn: A callable accepting the output of the `get_model_weights`
+      attribute of the iterative process, and a list of evaluation client
+      datasets. Returns a dictionary of evaluation metrics. Used to compute
+      evaluation metrics throughout the training process.
+    total_rounds: The number of federated training rounds to perform.
+    experiment_name: The name of the experiment being run. This will be appended
+      to the `root_output_dir` for purposes of writing outputs.
+    test_fn: A callable accepting the output of the `get_model_weights`
+      attribute of the iterative process and returning a dictionary of test
+      metrics. Used to compute test metrics at the end of the training process.
+    root_output_dir: The name of the root output directory for writing
+      experiment outputs.
+    hparam_dict: An optional dictionary specifying hyperparameters of the
+      experiment. If provided, the hyperparameters will be written to CSV.
+    rounds_per_eval: How often to compute validation metrics.
+    rounds_per_checkpoint: How often to checkpoint the iterative process state.
+      If you expect the job to restart frequently, this should be small. If no
+      interruptions are expected, this can be made larger.
+
+  Returns:
+    The final `state` of the iterative process after training.
+  """
+  _check_iterative_process_compatibility(iterative_process)
+  if not callable(train_client_datasets_fn):
+    raise TypeError('train_client_datasets_fn should be callable.')
+  if not callable(eval_client_datasets_fn):
+    raise TypeError('eval_client_datasets_fn should be callable.')
+  if not callable(evaluation_fn):
+    raise TypeError('evaluation_fn should be callable.')
+  if test_fn is not None and not callable(test_fn):
+    raise TypeError('test_fn should be callable.')
+
+  logging.info('Starting iterative_process training loop...')
+  initial_state = iterative_process.initialize()
+
+  if not hasattr(initial_state, 'model'):
+    raise TypeError('The server state must have a model attribute.')
+
+  checkpoint_mngr, metrics_mngr, summary_writer = _setup_outputs(
+      root_output_dir, experiment_name, hparam_dict)
+
+  logging.info('Asking checkpoint manager to load checkpoint.')
+  state, round_num = checkpoint_mngr.load_latest_checkpoint(initial_state)
+
+  if state is None:
+    logging.info('Initializing experiment from scratch.')
+    state = initial_state
+    round_num = 0
+    metrics_mngr.clear_all_rounds()
+  else:
+    logging.info('Restarted from checkpoint round %d', round_num)
+    round_num += 1  # Increment to avoid overwriting current checkpoint
+    metrics_mngr.clear_rounds_after(last_valid_round_num=round_num - 1)
+
+  loop_start_time = time.time()
+  while round_num < total_rounds:
+    data_prep_start_time = time.time()
+    federated_train_data = train_client_datasets_fn(round_num)
+    train_metrics = {
+        'prepare_datasets_secs': time.time() - data_prep_start_time
+    }
+
+    training_start_time = time.time()
+    prev_model = iterative_process.get_model_weights(state)
+    state, round_metrics = iterative_process.next(state, federated_train_data)
+    current_model = iterative_process.get_model_weights(state)
+
+    train_metrics['training_secs'] = time.time() - training_start_time
+    train_metrics['model_delta_l2_norm'] = _compute_numpy_l2_difference(
+        current_model, prev_model)
+    train_metrics.update(round_metrics)
+
+    logging.info('Round {:2d}, {:.2f}s per round in average.'.format(
+        round_num, (time.time() - loop_start_time) / (round_num + 1)))
+
+    if (round_num % rounds_per_checkpoint == 0 or
+        round_num == total_rounds - 1):
+      save_checkpoint_start_time = time.time()
+      checkpoint_mngr.save_checkpoint(state, round_num)
+      train_metrics['save_checkpoint_secs'] = (
+          time.time() - save_checkpoint_start_time)
+
+    metrics = {'train': train_metrics}
+
+    if round_num % rounds_per_eval == 0:
+      # Compute evaluation metrics
+      evaluate_start_time = time.time()
+      federated_eval_data = eval_client_datasets_fn(round_num)
+      validation_metrics = evaluation_fn(current_model, federated_eval_data)
+      validation_metrics['evaluate_secs'] = time.time() - evaluate_start_time
+      metrics['eval'] = validation_metrics
+
+    _write_metrics(metrics_mngr, summary_writer, metrics, round_num)
+    round_num += 1
+
+  # Final metrics evaluation once the training has completed
+  metrics = {}
+
+  # Evaluation metrics
+  evaluate_start_time = time.time()
+  federated_eval_data = eval_client_datasets_fn(round_num)
+  validation_metrics = evaluation_fn(current_model, federated_eval_data)
+  validation_metrics['evaluate_secs'] = time.time() - evaluate_start_time
+  metrics['eval'] = validation_metrics
+
+  # Test set metrics
+  if test_fn:
+    test_start_time = time.time()
+    test_metrics = test_fn(current_model)
+    test_metrics['evaluate_secs'] = time.time() - test_start_time
+    metrics['test'] = test_metrics
+  _write_metrics(metrics_mngr, summary_writer, metrics, total_rounds)
+
+  return state
---
+++ fedopt_guide/training_loop_test.py
@@ -0,0 +1,399 @@
+# Copyright 2020, Google LLC.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import collections
+import os
+
+import numpy as np
+import tensorflow as tf
+import tensorflow_federated as tff
+
+from fedopt_guide import training_loop
+from utils import checkpoint_manager
+from utils import metrics_manager
+from utils import training_utils
+
+_Batch = collections.namedtuple('Batch', ['x', 'y'])
+
+_COMPATIBILITY_ERROR_MESSAGE = (
+    'The iterative_process argument must be of '
+    'type`tff.templates.IterativeProcess`, and must have an '
+    'attribute `get_model_weights`, which must be a `tff.Computation`. This '
+    'computation must accept as input the state of `iterative_process`, and '
+    'its output must be a nested structure of tensors matching the expected '
+    'shape of the first input argument to `evaluation_fn`.')
+
+
+def _keras_model_builder():
+  # Create a simple linear regression model, single output.
+  model = tf.keras.Sequential([
+      tf.keras.layers.Dense(
+          1,
+          kernel_initializer='zeros',
+          bias_initializer='zeros',
+          input_shape=(1,))
+  ])
+  return model
+
+
+def _compiled_keras_model_builder():
+  model = _keras_model_builder()
+  model.compile(loss=tf.keras.losses.MeanSquaredError())
+  return model
+
+
+def _create_tf_dataset_for_client(client_id, batch_data=True):
+  # Create client data for y = 2*x+3
+  np.random.seed(client_id)
+  x = np.random.rand(6, 1).astype(np.float32)
+  y = 2 * x + 3
+  dataset = tf.data.Dataset.from_tensor_slices(
+      collections.OrderedDict([('x', x), ('y', y)]))
+  if batch_data:
+    dataset = dataset.batch(2)
+  return dataset
+
+
+def _get_input_spec():
+  return _create_tf_dataset_for_client(0).element_spec
+
+
+def _tff_model_fn():
+  return tff.learning.from_keras_model(
+      keras_model=_keras_model_builder(),
+      input_spec=_get_input_spec(),
+      loss=tf.keras.losses.MeanSquaredError(),
+      metrics=[tf.keras.metrics.MeanSquaredError()])
+
+
+def _build_federated_averaging_process():
+  return tff.learning.build_federated_averaging_process(
+      _tff_model_fn,
+      client_optimizer_fn=tf.keras.optimizers.SGD,
+      server_optimizer_fn=tf.keras.optimizers.SGD)
+
+
+def _evaluation_fn():
+  metrics_builder = lambda: [tf.keras.metrics.SparseCategoricalAccuracy()]
+  return training_utils.build_federated_evaluate_fn(
+      model_builder=_keras_model_builder, metrics_builder=metrics_builder)
+
+
+def _federated_data():
+  return [_create_tf_dataset_for_client(1), _create_tf_dataset_for_client(2)]
+
+
+class TrainingLoopArgumentsTest(tf.test.TestCase):
+
+  def test_raises_non_iterative_process(self):
+    bad_iterative_process = _build_federated_averaging_process().next
+
+    def client_datasets_fn(round_num):
+      del round_num
+      return _federated_data()
+
+    def evaluation_fn(model, eval_data):
+      del model, eval_data
+      return {}
+
+    root_output_dir = self.get_temp_dir()
+    with self.assertRaises(TypeError):
+      training_loop.run(
+          iterative_process=[bad_iterative_process],
+          train_client_datasets_fn=client_datasets_fn,
+          eval_client_datasets_fn=client_datasets_fn,
+          evaluation_fn=evaluation_fn,
+          total_rounds=10,
+          experiment_name='non_iterative_process',
+          root_output_dir=root_output_dir)
+
+  def test_raises_non_callable_train_client_dataset(self):
+    iterative_process = _build_federated_averaging_process()
+    client_dataset = _create_tf_dataset_for_client(3)
+    eval_client_datasets_fn = lambda round_num: [client_dataset]
+
+    def evaluation_fn(model):
+      del model
+      return {}
+
+    root_output_dir = self.get_temp_dir()
+    with self.assertRaises(TypeError):
+      training_loop.run(
+          iterative_process=iterative_process,
+          train_client_datasets_fn=client_dataset,
+          eval_client_datasets_fn=eval_client_datasets_fn,
+          evaluation_fn=evaluation_fn,
+          total_rounds=10,
+          experiment_name='non_callable_client_dataset',
+          root_output_dir=root_output_dir)
+
+  def test_raises_non_callable_evaluate_fn(self):
+    iterative_process = _build_federated_averaging_process()
+
+    def client_datasets_fn(round_num):
+      del round_num
+      return _federated_data()
+
+    metrics_dict = {}
+    root_output_dir = self.get_temp_dir()
+    with self.assertRaises(TypeError):
+      training_loop.run(
+          iterative_process=iterative_process,
+          train_client_datasets_fn=client_datasets_fn,
+          eval_client_datasets_fn=client_datasets_fn,
+          evaluation_fn=metrics_dict,
+          total_rounds=10,
+          experiment_name='non_callable_evaluate',
+          root_output_dir=root_output_dir)
+
+  def test_raises_non_str_output_dir(self):
+    iterative_process = _build_federated_averaging_process()
+
+    def client_datasets_fn(round_num):
+      del round_num
+      return _federated_data()
+
+    def evaluation_fn(model, eval_data):
+      del model, eval_data
+      return {}
+
+    with self.assertRaises(TypeError):
+      training_loop.run(
+          iterative_process=iterative_process,
+          train_client_datasets_fn=client_datasets_fn,
+          eval_client_datasets_fn=client_datasets_fn,
+          evaluation_fn=evaluation_fn,
+          total_rounds=10,
+          experiment_name='non_str_output_dir',
+          root_output_dir=1)
+
+  def test_raises_no_get_model_weights_attribute_in_state(self):
+
+    class BadIterativeProcess(tff.templates.IterativeProcess):
+
+      def __init__(self):
+        pass
+
+      def initialize(self):
+        return {}
+
+      def next(self, state, data):
+        return {}
+
+    iterative_process = BadIterativeProcess()
+
+    def client_datasets_fn(round_num):
+      del round_num
+      return _federated_data()
+
+    def evaluation_fn(model, eval_data):
+      del model, eval_data
+      return {}
+
+    with self.assertRaisesRegex(
+        training_loop.IterativeProcessCompatibilityError,
+        _COMPATIBILITY_ERROR_MESSAGE):
+      training_loop.run(
+          iterative_process=iterative_process,
+          train_client_datasets_fn=client_datasets_fn,
+          eval_client_datasets_fn=client_datasets_fn,
+          evaluation_fn=evaluation_fn,
+          total_rounds=10,
+          experiment_name='bad_iterative_process')
+
+  def test_raises_non_callable_get_model_weights_attribute(self):
+
+    class BadIterativeProcess(tff.templates.IterativeProcess):
+
+      def __init__(self):
+        pass
+
+      def initialize(self):
+        return {}
+
+      def next(self, state, data):
+        return {}
+
+    iterative_process = BadIterativeProcess()
+    iterative_process.get_model_weights = 2
+
+    def client_datasets_fn(round_num):
+      del round_num
+      return _federated_data()
+
+    def evaluation_fn(model, eval_data):
+      del model, eval_data
+      return {}
+
+    with self.assertRaisesRegex(
+        training_loop.IterativeProcessCompatibilityError,
+        _COMPATIBILITY_ERROR_MESSAGE):
+      training_loop.run(
+          iterative_process=iterative_process,
+          train_client_datasets_fn=client_datasets_fn,
+          eval_client_datasets_fn=client_datasets_fn,
+          evaluation_fn=evaluation_fn,
+          total_rounds=10,
+          experiment_name='bad_iterative_process')
+
+  def test_raises_non_tff_computation_get_model_weights_attribute(self):
+
+    class BadIterativeProcess(tff.templates.IterativeProcess):
+
+      def __init__(self):
+        pass
+
+      def initialize(self):
+        return {}
+
+      def next(self, state, data):
+        return {}
+
+      def get_model_weights(self, state):
+        return {}
+
+    iterative_process = BadIterativeProcess()
+
+    def client_datasets_fn(round_num):
+      del round_num
+      return _federated_data()
+
+    def evaluation_fn(model, eval_data):
+      del model, eval_data
+      return {}
+
+    with self.assertRaisesRegex(
+        training_loop.IterativeProcessCompatibilityError,
+        _COMPATIBILITY_ERROR_MESSAGE):
+
+      training_loop.run(
+          iterative_process=iterative_process,
+          train_client_datasets_fn=client_datasets_fn,
+          eval_client_datasets_fn=client_datasets_fn,
+          evaluation_fn=evaluation_fn,
+          total_rounds=10,
+          experiment_name='bad_iterative_process')
+
+
+class ExperimentRunnerTest(tf.test.TestCase):
+
+  def test_fedavg_training_decreases_loss(self):
+    iterative_process = _build_federated_averaging_process()
+
+    def client_datasets_fn(round_num):
+      del round_num
+      return _federated_data()
+
+    def test_fn(model):
+      keras_model = _compiled_keras_model_builder()
+      model.assign_weights_to(keras_model)
+      batch = next(iter(_create_tf_dataset_for_client(5)))
+      return {'loss': keras_model.evaluate(batch['x'], batch['y'])}
+
+    initial_state = iterative_process.initialize()
+    initial_model = iterative_process.get_model_weights(initial_state)
+
+    root_output_dir = self.get_temp_dir()
+    final_state = training_loop.run(
+        iterative_process=iterative_process,
+        train_client_datasets_fn=client_datasets_fn,
+        eval_client_datasets_fn=client_datasets_fn,
+        evaluation_fn=_evaluation_fn(),
+        total_rounds=1,
+        experiment_name='fedavg_decreases_loss',
+        root_output_dir=root_output_dir)
+    final_model = iterative_process.get_model_weights(final_state)
+    self.assertLess(
+        test_fn(final_model)['loss'],
+        test_fn(initial_model)['loss'])
+
+  def test_checkpoint_manager_saves_state(self):
+    experiment_name = 'checkpoint_manager_saves_state'
+    iterative_process = _build_federated_averaging_process()
+
+    def client_datasets_fn(round_num):
+      del round_num
+      return _federated_data()
+
+    def evaluation_fn(model, eval_data):
+      del model, eval_data
+      return {}
+
+    root_output_dir = self.get_temp_dir()
+    final_state = training_loop.run(
+        iterative_process=iterative_process,
+        train_client_datasets_fn=client_datasets_fn,
+        eval_client_datasets_fn=client_datasets_fn,
+        evaluation_fn=evaluation_fn,
+        total_rounds=1,
+        experiment_name=experiment_name,
+        root_output_dir=root_output_dir)
+    final_model = iterative_process.get_model_weights(final_state)
+
+    ckpt_manager = checkpoint_manager.FileCheckpointManager(
+        os.path.join(root_output_dir, 'checkpoints', experiment_name))
+    restored_state, restored_round = ckpt_manager.load_latest_checkpoint(
+        final_state)
+
+    self.assertEqual(restored_round, 0)
+
+    keras_model = _compiled_keras_model_builder()
+    restored_model = iterative_process.get_model_weights(restored_state)
+
+    restored_model.assign_weights_to(keras_model)
+    batch = next(iter(_create_tf_dataset_for_client(5)))
+    restored_loss = keras_model.test_on_batch(batch['x'], batch['y'])
+
+    final_model.assign_weights_to(keras_model)
+    final_loss = keras_model.test_on_batch(batch['x'], batch['y'])
+    self.assertEqual(final_loss, restored_loss)
+
+  def test_fn_writes_metrics(self):
+    experiment_name = 'test_metrics'
+    iterative_process = _build_federated_averaging_process()
+
+    def client_datasets_fn(round_num):
+      del round_num
+      return _federated_data()
+
+    def test_fn(model):
+      keras_model = _compiled_keras_model_builder()
+      model.assign_weights_to(keras_model)
+      batch = next(iter(_create_tf_dataset_for_client(5)))
+      return {'loss': keras_model.evaluate(batch['x'], batch['y'])}
+
+    root_output_dir = self.get_temp_dir()
+    training_loop.run(
+        iterative_process=iterative_process,
+        train_client_datasets_fn=client_datasets_fn,
+        eval_client_datasets_fn=client_datasets_fn,
+        evaluation_fn=_evaluation_fn(),
+        total_rounds=1,
+        experiment_name=experiment_name,
+        root_output_dir=root_output_dir,
+        rounds_per_eval=10,
+        test_fn=test_fn)
+
+    results_dir = os.path.join(root_output_dir, 'results', experiment_name)
+
+    scalar_manager = metrics_manager.ScalarMetricsManager(results_dir)
+    fieldnames, metrics = scalar_manager.get_metrics()
+    self.assertLen(metrics, 2)
+    self.assertIn('eval/sparse_categorical_accuracy/example_weighted',
+                  fieldnames)
+    self.assertIn('test/loss', fieldnames)
+
+
+if __name__ == '__main__':
+  tf.test.main()
