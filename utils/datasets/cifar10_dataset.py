90d870064ad462af313f44e6b64191981789f7d3
AdvaitGadhikar <gadhikaradvait@gmail.com>
2021-01-10 23:23:04 -0500

    added the latest version

--- utils/datasets/cifar10_dataset.py
+++ utils/datasets/cifar10_dataset.py
@@ -1,4 +1,4 @@
-# Copyright 2019, Google LLC.
+# Copyright 2021, Google LLC.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,11 +14,12 @@
 """Library for loading and preprocessing CIFAR-10 training and testing data."""
 
 import collections
-from typing import Tuple
+from typing import Callable, Sequence, Tuple, Union
+
 import numpy as np
 import tensorflow as tf
+import tensorflow_datasets as tfds
 import tensorflow_federated as tff
-from tensorflow.keras import datasets, layers, models
 
 CIFAR_SHAPE = (32, 32, 3)
 TOTAL_FEATURE_SIZE = 32 * 32 * 3
@@ -27,112 +28,138 @@ TEST_SAMPLES_PER_CLIENT = 1000
 NUM_CLASSES = 10
 NUM_CLIENTS = 10
 
-def load_cifar10_federated(dirichlet_parameter: float = 1):
-  '''Construct a synthetic federated CIFAR-10 from its centralized version
-  Loads the train dataset into a non iid distribution over clients using the
-  sampling method based on LDA, taken from this paper:
+
+def load_cifar10_federated(
+    dirichlet_parameter: float = 1
+) -> Tuple[tff.simulation.ClientData, tff.simulation.ClientData]:
+  """Construct a federated dataset from the centralized CIFAR-10.
+  Sampling based on Dirichlet distribution over categories, following the paper
   Measuring the Effects of Non-Identical Data Distribution for
-  Federated Visual Classification (https://arxiv.org/pdf/1909.06335.pdf).
+  Federated Visual Classification (https://arxiv.org/abs/1909.06335).
   Args:
-    dirichlet_parameter: A float parametrizing the Dirichlet distribution which samples a 
-      multinomial over classes for each client.
+    dirichlet_parameter: Parameter of Dirichlet distribution. Each client
+      samples from this Dirichlet to get a multinomial distribution over
+      classes. It controls the data heterogeneity of clients. If approaches 0,
+      then each client only have data from a single category label. If
+      approaches infinity, then the client distribution will approach IID
+      partitioning.
   Returns:
     A tuple of `tff.simulation.ClientData` representing unpreprocessed
     train data and test data.
-  '''
-  (train_images, train_labels), (test_images, test_labels) \
-      = datasets.cifar10.load_data()
+  """
+  train_images, train_labels = tfds.as_numpy(
+      tfds.load(
+          name='cifar10',
+          split='train',
+          batch_size=-1,
+          as_supervised=True,
+      ))
+  test_images, test_labels = tfds.as_numpy(
+      tfds.load(
+          name='cifar10',
+          split='test',
+          batch_size=-1,
+          as_supervised=True,
+      ))
   train_clients = collections.OrderedDict()
   test_clients = collections.OrderedDict()
 
-  
   train_multinomial_vals = []
   test_multinomial_vals = []
-  # each column is a distribution over classes for the client at that index
-  for i in range(num_clients):
-      proportion = np.random.dirichlet(dirichlet_parameter*np.ones(NUM_CLASSES,))
-      train_multinomial_vals.append(proportion)
-      test_multinomial_vals.append(proportion)
-        
+  # Each client has a multinomial distribution over classes drawn from a
+  # Dirichlet.
+  for i in range(NUM_CLIENTS):
+    proportion = np.random.dirichlet(dirichlet_parameter *
+                                     np.ones(NUM_CLASSES,))
+    train_multinomial_vals.append(proportion)
+    test_multinomial_vals.append(proportion)
+
   train_multinomial_vals = np.array(train_multinomial_vals)
   test_multinomial_vals = np.array(test_multinomial_vals)
-  
-  train_indices = []
+
+  train_example_indices = []
   test_indices = []
   for k in range(NUM_CLASSES):
-    train_label_k = np.where(train_labels==k)[0]
+    train_label_k = np.where(train_labels == k)[0]
     np.random.shuffle(train_label_k)
-    train_indices.append(train_label_k)
-    test_label_k = np.where(test_labels==k)[0]
+    train_example_indices.append(train_label_k)
+    test_label_k = np.where(test_labels == k)[0]
     np.random.shuffle(test_label_k)
     test_indices.append(test_label_k)
 
-  train_indices = np.array(train_indices)
+  train_example_indices = np.array(train_example_indices)
   test_indices = np.array(test_indices)
 
   train_client_samples = [[] for _ in range(NUM_CLIENTS)]
   test_client_samples = [[] for _ in range(NUM_CLIENTS)]
-  '''
-  sample 5000 images for the current client according 
-  to the multinomial without replacement
-  '''
   train_count = np.zeros(NUM_CLASSES).astype(int)
   test_count = np.zeros(NUM_CLASSES).astype(int)
 
   for k in range(NUM_CLASSES):
-        
+
     for i in range(NUM_EXAMPLES_PER_CLIENT):
-      sampled_label = np.argwhere(np.random.multinomial(1, train_multinomial_vals[k,:])==1)[0][0]
-      train_client_samples[k].append(train_indices[sampled_label, train_count[sampled_label]])
+      sampled_label = np.argwhere(
+          np.random.multinomial(1, train_multinomial_vals[k, :]) == 1)[0][0]
+      train_client_samples[k].append(
+          train_example_indices[sampled_label, train_count[sampled_label]])
       train_count[sampled_label] += 1
       if train_count[sampled_label] == NUM_EXAMPLES_PER_CLIENT:
         train_multinomial_vals[:, sampled_label] = 0
-        train_multinomial_vals = train_multinomial_vals / train_multinomial_vals.sum(axis=1)[:, None]
+        train_multinomial_vals = (
+            train_multinomial_vals /
+            train_multinomial_vals.sum(axis=1)[:, None])
 
     for i in range(TEST_SAMPLES_PER_CLIENT):
-      sampled_label = np.argwhere(np.random.multinomial(1, test_multinomial_vals[k,:])==1)[0][0]
-      test_client_samples[k].append(test_indices[sampled_label, test_count[sampled_label]])
+      sampled_label = np.argwhere(
+          np.random.multinomial(1, test_multinomial_vals[k, :]) == 1)[0][0]
+      test_client_samples[k].append(test_indices[sampled_label,
+                                                 test_count[sampled_label]])
       test_count[sampled_label] += 1
       if test_count[sampled_label] == TEST_SAMPLES_PER_CLIENT:
         test_multinomial_vals[:, sampled_label] = 0
-        test_multinomial_vals = test_multinomial_vals / test_multinomial_vals.sum(axis=1)[:, None]
-
+        test_multinomial_vals = (
+            test_multinomial_vals / test_multinomial_vals.sum(axis=1)[:, None])
 
   for i in range(NUM_CLIENTS):
     client_name = str(i)
     x_train = train_images[np.array(train_client_samples[i])]
-    y_train = train_labels[np.array(train_client_samples[i])].astype('int64').squeeze()
-    train_data = collections.OrderedDict((('image', x_train), ('label', y_train)))
+    y_train = train_labels[np.array(
+        train_client_samples[i])].astype('int64').squeeze()
+    train_data = collections.OrderedDict(
+        (('image', x_train), ('label', y_train)))
     train_clients[client_name] = train_data
-    
+
     x_test = test_images[np.array(test_client_samples[i])]
-    y_test = test_labels[np.array(test_client_samples[i])].astype('int64').squeeze()
+    y_test = test_labels[np.array(
+        test_client_samples[i])].astype('int64').squeeze()
     test_data = collections.OrderedDict((('image', x_test), ('label', y_test)))
     test_clients[client_name] = test_data
-    
-    
+
   train_dataset = tff.simulation.FromTensorSlicesClientData(train_clients)
   test_dataset = tff.simulation.FromTensorSlicesClientData(test_clients)
 
   return train_dataset, test_dataset
 
 
-def build_image_map(crop_shape, distort=False):
+def build_image_map(
+    crop_shape: Union[tf.Tensor, Sequence[int]],
+    distort: bool = False
+) -> Callable[[tf.Tensor], Tuple[tf.Tensor, tf.Tensor]]:
   """Builds a function that crops and normalizes CIFAR-10 elements.
   The image is first converted to a `tf.float32`, then cropped (according to
   the `distort` argument). Finally, its values are normalized via
   `tf.image.per_image_standardization`.
   Args:
-    crop_shape: A tuple (crop_height, crop_width, num_channels) specifying the
-      desired crop shape for pre-processing. This tuple cannot have elements
-      exceeding (32, 32, 3), element-wise. The element in the last index should
-      be set to 3 to maintain the RGB image structure of the elements.
+    crop_shape: A tuple (crop_height, crop_width, channels) specifying the
+      desired crop shape for pre-processing batches. This cannot exceed (32, 32,
+      3) element-wise. The element in the last index should be set to 3 to
+      maintain the RGB image structure of the elements.
     distort: A boolean indicating whether to distort the image via random crops
       and flips. If set to False, the image is resized to the `crop_shape` via
       `tf.image.resize_with_crop_or_pad`.
   Returns:
-    A callable accepting a tensor of shape (32, 32, 3), and performing the
-    crops and normalization discussed above.
+    A callable accepting a tensor and performing the crops and normalization
+    discussed above.
   """
 
   if distort:
@@ -146,7 +173,7 @@ def build_image_map(crop_shape, distort=False):
 
     def crop_fn(image):
       return tf.image.resize_with_crop_or_pad(
-          image, target_height=crop_shape[1], target_width=crop_shape[2])
+          image, target_height=crop_shape[0], target_width=crop_shape[1])
 
   def image_map(example):
     image = tf.cast(example['image'], tf.float32)
@@ -195,8 +222,13 @@ def create_preprocess_fn(
 
   @tff.tf_computation(tff.SequenceType(feature_dtypes))
   def preprocess_fn(dataset):
-    return (dataset.shuffle(shuffle_buffer_size).repeat(num_epochs).batch(
-        batch_size).map(image_map_fn, num_parallel_calls=num_parallel_calls))
+    return (
+        dataset.shuffle(shuffle_buffer_size).repeat(num_epochs)
+        # We map before batching to ensure that the cropping occurs
+        # at an image level (eg. we do not perform the same crop on
+        # every image within a batch)
+        .map(image_map_fn,
+             num_parallel_calls=num_parallel_calls).batch(batch_size))
 
   return preprocess_fn
 
@@ -210,7 +242,7 @@ def get_federated_datasets(
     test_shuffle_buffer_size: int = 1,
     crop_shape: Tuple[int, int, int] = CIFAR_SHAPE,
     serializable: bool = False):
-  """Loads and preprocesses federated CIFAR100 training and testing sets.
+  """Loads and preprocesses federated CIFAR10 training and testing sets.
   Args:
     train_client_batch_size: The batch size for all train clients.
     test_client_batch_size: The batch size for all test clients.
@@ -227,11 +259,11 @@ def get_federated_datasets(
     test_shuffle_buffer_size: An integer representing the shuffle buffer size
       (as in `tf.data.Dataset.shuffle`) for each test client's dataset. If set
       to some integer less than or equal to 1, no shuffling occurs.
-    crop_shape: An iterable of integers specifying the desired crop
-      shape for pre-processing. Must be convertable to a tuple of integers
-      (CROP_HEIGHT, CROP_WIDTH, NUM_CHANNELS) which cannot have elements that
-      exceed (32, 32, 3), element-wise. The element in the last index should be
-      set to 3 to maintain the RGB image structure of the elements.
+    crop_shape: An iterable of integers specifying the desired crop shape for
+      pre-processing. Must be convertable to a tuple of integers (CROP_HEIGHT,
+      CROP_WIDTH, NUM_CHANNELS) which cannot have elements that exceed (32, 32,
+      3), element-wise. The element in the last index should be set to 3 to
+      maintain the RGB image structure of the elements.
     serializable: Boolean indicating whether the returned datasets are intended
       to be serialized and shipped across RPC channels. If `True`, stateful
       transformations will be disallowed.
@@ -239,7 +271,7 @@ def get_federated_datasets(
     A tuple (cifar_train, cifar_test) of `tff.simulation.ClientData` instances
       representing the federated training and test datasets.
   """
-  if not isinstance(crop_shape, collections.Iterable):
+  if not isinstance(crop_shape, collections.abc.Iterable):
     raise TypeError('Argument crop_shape must be an iterable.')
   crop_shape = tuple(crop_shape)
   if len(crop_shape) != 3:
@@ -260,21 +292,19 @@ def get_federated_datasets(
     test_shuffle_buffer_size = 1
 
   cifar_train, cifar_test = load_cifar10_federated()
-  train_crop_shape = (train_client_batch_size,) + crop_shape
-  test_crop_shape = (test_client_batch_size,) + crop_shape
 
   train_preprocess_fn = create_preprocess_fn(
       num_epochs=train_client_epochs_per_round,
       batch_size=train_client_batch_size,
       shuffle_buffer_size=train_shuffle_buffer_size,
-      crop_shape=train_crop_shape,
+      crop_shape=crop_shape,
       distort_image=not serializable)
 
   test_preprocess_fn = create_preprocess_fn(
       num_epochs=test_client_epochs_per_round,
       batch_size=test_client_batch_size,
       shuffle_buffer_size=test_shuffle_buffer_size,
-      crop_shape=test_crop_shape,
+      crop_shape=crop_shape,
       distort_image=False)
 
   cifar_train = cifar_train.preprocess(train_preprocess_fn)
@@ -284,12 +314,12 @@ def get_federated_datasets(
 
 def get_centralized_datasets(
     train_batch_size: int = 20,
-    test_batch_size: int = 20,
+    test_batch_size: int = 100,
     train_shuffle_buffer_size: int = 10000,
     test_shuffle_buffer_size: int = 1,
     crop_shape: Tuple[int, int, int] = CIFAR_SHAPE
 ) -> Tuple[tf.data.Dataset, tf.data.Dataset]:
-  """Loads and preprocesses centralized CIFAR100 training and testing sets.
+  """Loads and preprocesses centralized CIFAR10 training and testing sets.
   Args:
     train_batch_size: The batch size for the training dataset.
     test_batch_size: The batch size for the test dataset.
@@ -299,14 +329,13 @@ def get_centralized_datasets(
     test_shuffle_buffer_size: An integer specifying the buffer size used to
       shuffle the test dataset via `tf.data.Dataset.shuffle`. If set to an
       integer less than or equal to 1, no shuffling occurs.
-    crop_shape: An iterable of integers specifying the desired crop
-      shape for pre-processing. Must be convertable to a tuple of integers
-      (CROP_HEIGHT, CROP_WIDTH, NUM_CHANNELS) which cannot have elements that
-      exceed (32, 32, 3), element-wise. The element in the last index should be
-      set
+    crop_shape: An iterable of integers specifying the desired crop shape for
+      pre-processing. Must be convertable to a tuple of integers (CROP_HEIGHT,
+      CROP_WIDTH, NUM_CHANNELS) which cannot have elements that exceed (32, 32,
+      3), element-wise. The element in the last index should be set to 3 to
+      maintain the RGB image structure of the elements.
   Returns:
-    A tup
-le (cifar_train, cifar_test) of `tf.data.Dataset` instances
+    A tuple (cifar_train, cifar_test) of `tf.data.Dataset` instances
     representing the centralized training and test datasets.
   """
   try:
@@ -326,14 +355,11 @@ le (cifar_train, cifar_test) of `tf.data.Dataset` instances
   cifar_train = cifar_train.create_tf_dataset_from_all_clients()
   cifar_test = cifar_test.create_tf_dataset_from_all_clients()
 
-  train_crop_shape = (train_batch_size,) + crop_shape
-  test_crop_shape = (test_batch_size,) + crop_shape
-
   train_preprocess_fn = create_preprocess_fn(
       num_epochs=1,
       batch_size=train_batch_size,
       shuffle_buffer_size=train_shuffle_buffer_size,
-      crop_shape=train_crop_shape,
+      crop_shape=crop_shape,
       distort_image=True)
   cifar_train = train_preprocess_fn(cifar_train)
 
@@ -341,7 +367,7 @@ le (cifar_train, cifar_test) of `tf.data.Dataset` instances
       num_epochs=1,
       batch_size=test_batch_size,
       shuffle_buffer_size=test_shuffle_buffer_size,
-      crop_shape=test_crop_shape,
+      crop_shape=crop_shape,
       distort_image=False)
   cifar_test = test_preprocess_fn(cifar_test)
 
--- utils/datasets/cifar10_dataset_test.py
+++ utils/datasets/cifar10_dataset_test.py
@@ -1,4 +1,4 @@
-# Copyright 2019, Google LLC.
+# Copyright 2021, Google LLC.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,53 +14,115 @@
 
 import collections
 
+from absl.testing import parameterized
 import tensorflow as tf
-import tensorflow_datasets as tfds
 
 from utils.datasets import cifar10_dataset
+
+
+TEST_DATA = collections.OrderedDict(
+    image=([tf.zeros((32, 32, 3), dtype=tf.uint8)]),
+    label=([tf.constant(1, dtype=tf.int64)]),
+)
 
 
 def _compute_length_of_dataset(ds):
   return ds.reduce(0, lambda x, _: x + 1)
 
 
-class DatasetTest(tf.test.TestCase):
+class PreprocessFnTest(tf.test.TestCase, parameterized.TestCase):
 
-  '''
-  A Test to check the dataset has the correct number 
-  of clients, assuming the number of clients is 10
-  '''
+  @parameterized.named_parameters(
+      ('crop_shape_1_no_distort', (32, 32, 3), False),
+      ('crop_shape_2_no_distort', (28, 28, 3), False),
+      ('crop_shape_3_no_distort', (24, 26, 3), False),
+      ('crop_shape_1_distort', (32, 32, 3), True),
+      ('crop_shape_2_distort', (28, 28, 3), True),
+      ('crop_shape_3_distort', (24, 26, 3), True),
+  )
+  def test_preprocess_element_spec(self, crop_shape, distort_image):
+    ds = tf.data.Dataset.from_tensor_slices(TEST_DATA)
+    preprocess_fn = cifar10_dataset.create_preprocess_fn(
+        num_epochs=1,
+        batch_size=1,
+        shuffle_buffer_size=1,
+        crop_shape=crop_shape,
+        distort_image=distort_image)
+    preprocessed_ds = preprocess_fn(ds)
+    expected_element_shape = (None,) + crop_shape
+    self.assertEqual(
+        preprocessed_ds.element_spec,
+        (tf.TensorSpec(shape=expected_element_shape, dtype=tf.float32),
+         tf.TensorSpec(shape=(None,), dtype=tf.int64)))
+
+  @parameterized.named_parameters(
+      ('crop_shape_1_no_distort', (32, 32, 3), False),
+      ('crop_shape_2_no_distort', (28, 28, 3), False),
+      ('crop_shape_3_no_distort', (24, 26, 3), False),
+      ('crop_shape_1_distort', (32, 32, 3), True),
+      ('crop_shape_2_distort', (28, 28, 3), True),
+      ('crop_shape_3_distort', (24, 26, 3), True),
+  )
+  def test_preprocess_returns_correct_element(self, crop_shape, distort_image):
+    ds = tf.data.Dataset.from_tensor_slices(TEST_DATA)
+    preprocess_fn = cifar10_dataset.create_preprocess_fn(
+        num_epochs=1,
+        batch_size=20,
+        shuffle_buffer_size=1,
+        crop_shape=crop_shape,
+        distort_image=distort_image)
+    preprocessed_ds = preprocess_fn(ds)
+
+    expected_element_shape = (1,) + crop_shape
+    element = next(iter(preprocessed_ds))
+    expected_element = (tf.zeros(
+        shape=expected_element_shape,
+        dtype=tf.float32), tf.ones(shape=(1,), dtype=tf.int32))
+    self.assertAllClose(self.evaluate(element), expected_element)
+
+  def test_no_op_crop(self):
+    crop_shape = (1, 1, 3)
+    x = tf.constant([[[1.0, -1.0, 0.0]]])  # Has shape (1, 1, 3), mean 0
+    x = x / tf.math.reduce_std(x)  # x now has variance 1
+    simple_example = collections.OrderedDict(image=x, label=0)
+    image_map = cifar10_dataset.build_image_map(crop_shape, distort=False)
+    cropped_example = image_map(simple_example)
+
+    self.assertEqual(cropped_example[0].shape, crop_shape)
+    self.assertAllClose(x, cropped_example[0], rtol=1e-03)
+    self.assertEqual(cropped_example[1], 0)
+
+
+class LoadCifarTest(tf.test.TestCase):
+
   def test_num_clients(self):
-    cifar_train, cifar_test = get_federated_datasets(
-        train_client_batch_size = 20,
-        test_client_batch_size = 100
-      )
-    client_list = cifar_train.client_ids
-    num_clients = len(client_list)
-    self.assertEqual(num_clients, 10)
+    cifar_train, cifar_test = cifar10_dataset.load_cifar10_federated()
+    self.assertEqual(len(cifar_train.client_ids), 10)
+    self.assertEqual(len(cifar_test.client_ids), 10)
 
-  def test_num_batches_per_client(self):
-    cifar_train, cifar_test = get_federated_datasets(
-        train_client_batch_size = 20,
-        test_client_batch_size = 100
-      )
-    client_local_dataset = tfds.as_numpy(
-        cifar_train.create_tf_dataset_for_client('0'))
-    num_batches = 0
-    for element in client_local_dataset:
-      num_batches += 1
-    self.assertEqual(num_batches, 250)
+  def test_dataset_length(self):
+    cifar_train, cifar_test = cifar10_dataset.load_cifar10_federated()
+    self.assertEqual(
+        _compute_length_of_dataset(
+            cifar_train.create_tf_dataset_for_client('0')), 5000)
+    self.assertEqual(
+        _compute_length_of_dataset(
+            cifar_test.create_tf_dataset_for_client('0')), 1000)
 
-  def test_centralized_cifar_structure(self):
-    crop_shape = (24, 24, 3)
-    cifar_train, cifar_test = cifar10_dataset.get_centralized_datasets(
-        train_batch_size=20, test_batch_size=100, crop_shape=crop_shape)
-    train_batch = next(iter(cifar_train))
-    train_batch_shape = tuple(train_batch[0].shape)
-    self.assertEqual(train_batch_shape, (20, 24, 24, 3))
-    test_batch = next(iter(cifar_test))
-    test_batch_shape = tuple(test_batch[0].shape)
-    self.assertEqual(test_batch_shape, (100, 24, 24, 3))
+
+class FederatedDatasetTest(tf.test.TestCase):
+
+  def test_get_federated_datasets(self):
+    cifar_train, cifar_test = cifar10_dataset.get_federated_datasets(
+        train_client_batch_size=20, test_client_batch_size=100)
+    self.assertEqual(len(cifar_train.client_ids), 10)
+    self.assertEqual(len(cifar_test.client_ids), 10)
+    self.assertEqual(
+        _compute_length_of_dataset(
+            cifar_train.create_tf_dataset_for_client('0')), 250)
+    self.assertEqual(
+        _compute_length_of_dataset(
+            cifar_test.create_tf_dataset_for_client('0')), 10)
 
   def test_federated_cifar_structure(self):
     crop_shape = (28, 28, 3)
@@ -81,28 +143,32 @@ class DatasetTest(tf.test.TestCase):
     test_batch_shape = tuple(test_batch[0].shape)
     self.assertEqual(test_batch_shape, (5, 28, 28, 3))
 
-  def test_no_op_crop_process_cifar_example(self):
-    crop_shape = (1, 1, 1, 3)
-    x = tf.constant([[[[1.0, -1.0, 0.0]]]])  # Has shape (1, 1, 1, 3), mean 0
-    x = x / tf.math.reduce_std(x)  # x now has variance 1
-    simple_example = collections.OrderedDict(image=x, label=0)
-    image_map = cifar10_dataset.build_image_map(crop_shape, distort=False)
-    cropped_example = image_map(simple_example)
-
-    self.assertEqual(cropped_example[0].shape, crop_shape)
-    self.assertAllClose(x, cropped_example[0], rtol=1e-03)
-    self.assertEqual(cropped_example[1], 0)
-
   def test_raises_length_2_crop(self):
     with self.assertRaises(ValueError):
       cifar10_dataset.get_federated_datasets(crop_shape=(32, 32))
-    with self.assertRaises(ValueError):
-      cifar10_dataset.get_centralized_datasets(crop_shape=(32, 32))
 
   def test_raises_negative_epochs(self):
     with self.assertRaisesRegex(
         ValueError, 'client_epochs_per_round must be a positive integer.'):
       cifar10_dataset.get_federated_datasets(train_client_epochs_per_round=-1)
+
+
+class CentralizedDatasetTest(tf.test.TestCase):
+
+  def test_raises_length_2_crop(self):
+    with self.assertRaises(ValueError):
+      cifar10_dataset.get_centralized_datasets(crop_shape=(32, 32))
+
+  def test_centralized_cifar_structure(self):
+    crop_shape = (24, 24, 3)
+    cifar_train, cifar_test = cifar10_dataset.get_centralized_datasets(
+        train_batch_size=20, test_batch_size=100, crop_shape=crop_shape)
+    train_batch = next(iter(cifar_train))
+    train_batch_shape = tuple(train_batch[0].shape)
+    self.assertEqual(train_batch_shape, (20, 24, 24, 3))
+    test_batch = next(iter(cifar_test))
+    test_batch_shape = tuple(test_batch[0].shape)
+    self.assertEqual(test_batch_shape, (100, 24, 24, 3))
 
 
 if __name__ == '__main__':
