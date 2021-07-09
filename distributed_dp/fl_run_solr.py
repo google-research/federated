8c412d276f42304e8565e8a43c6d3058a73b30db
Zachary Charles <zachcharles@google.com>
2021-07-08 20:28:04 -0700

    Convert code in `distributed_dp/fl_run_solr` to use tff.simulation.baselines.
    PiperOrigin-RevId: 383766926
    Change-Id: Ibc8390db60befea7d4877f1c35339cb2eccbab84

--- distributed_dp/fl_run_solr.py
+++ distributed_dp/fl_run_solr.py
@@ -13,10 +13,9 @@
 # limitations under the License.
 """Runs federated training with differential privacy on StackOverflow LR."""
 
+import functools
 import math
-import os.path
 import pprint
-from typing import Callable, List, Tuple
 
 from absl import app
 from absl import flags
@@ -24,13 +23,9 @@ from absl import logging
 import tensorflow_federated as tff
 
 from distributed_dp import fl_utils
-from optimization.tasks import stackoverflow_tp
-from optimization.tasks import training_specs
+from utils import training_utils
 from utils import utils_impl
 from utils.optimizers import optimizer_utils
-
-# We use a separate run_file for EMNIST for now as we might update the model.
-_SUPPORTED_TASKS = ['stackoverflow_lr']
 
 with utils_impl.record_hparam_flags() as optimizer_flags:
   # Defining optimizer flags
@@ -82,47 +77,15 @@ with utils_impl.record_hparam_flags() as dp_flags:
   flags.DEFINE_enum('dp_mechanism', 'ddgauss', dp_mechanisms,
                     'Which DP mechanism to use.')
 
-# Task specification
-flags.DEFINE_enum('task', 'stackoverflow_lr', _SUPPORTED_TASKS,
-                  'Which task to perform federated training on.')
-
 FLAGS = flags.FLAGS
 
+NUM_TRAIN_CLIENTS = 342477
 
-def _configure_managers() -> Tuple[tff.simulation.FileCheckpointManager,
-                                   List[tff.simulation.MetricsManager]]:
-  """Configures checkpoint and metrics managers from flags."""
-  root_output_dir = FLAGS.root_output_dir
-  experiment_name = FLAGS.experiment_name
-  utils_impl.create_directory_if_not_exists(root_output_dir)
-
-  checkpoint_dir = os.path.join(root_output_dir, 'checkpoints', experiment_name)
-  utils_impl.create_directory_if_not_exists(checkpoint_dir)
-  checkpoint_manager = tff.simulation.FileCheckpointManager(
-      checkpoint_dir, step=FLAGS.rounds_per_checkpoint)
-
-  results_dir = os.path.join(root_output_dir, 'results', experiment_name)
-  utils_impl.create_directory_if_not_exists(results_dir)
-  csv_file = os.path.join(results_dir, 'experiment.metrics.csv')
-  csv_manager = tff.simulation.CSVMetricsManager(csv_file)
-
-  summary_dir = os.path.join(root_output_dir, 'logdir', experiment_name)
-  tensorboard_manager = tff.simulation.TensorBoardManager(summary_dir)
-
-  logging.info('Writing...')
-  logging.info('    checkpoints to: %s', checkpoint_dir)
-  logging.info('    CSV metrics to: %s', csv_file)
-  logging.info('    TensorBoard summaries to: %s', summary_dir)
-
-  return checkpoint_manager, [csv_manager, tensorboard_manager]
-
-
-def get_total_num_clients(task):
-  """Hardcode the total number of clients for the dataset."""
-  if task == 'stackoverflow_lr':
-    return 342477
-  else:
-    raise ValueError(f'Unsupported task: {task}')
+# Hard coded constants governing the behavior of the tag prediction task.
+WORD_VOCAB_SIZE = 10000
+TAG_VOCAB_SIZE = 500
+MAX_ELEMENTS_PER_TRAIN_CLIENT = 1000
+NUM_VALIDATION_EXAMPLES = 10000
 
 
 def main(argv):
@@ -136,64 +99,66 @@ def main(argv):
   compression_dict = utils_impl.lookup_flag_values(compression_flags)
   dp_dict = utils_impl.lookup_flag_values(dp_flags)
 
-  def iterative_process_builder(
-      model_fn: Callable[[], tff.learning.Model],
-  ) -> tff.templates.IterativeProcess:
-    """Creates an iterative process using a given TFF `model_fn`."""
+  train_client_spec = tff.simulation.baselines.ClientSpec(
+      num_epochs=FLAGS.client_epochs_per_round,
+      batch_size=FLAGS.client_batch_size,
+      max_elements=MAX_ELEMENTS_PER_TRAIN_CLIENT)
+  task = tff.simulation.baselines.stackoverflow.create_tag_prediction_task(
+      train_client_spec,
+      word_vocab_size=WORD_VOCAB_SIZE,
+      tag_vocab_size=TAG_VOCAB_SIZE)
 
-    model_trainable_variables = model_fn().trainable_variables
+  model_trainable_variables = task.model_fn().trainable_variables
 
-    # Most logic for deciding what to run is here.
-    aggregation_factory = fl_utils.build_aggregator(
-        compression_flags=compression_dict,
-        dp_flags=dp_dict,
-        num_clients=get_total_num_clients(FLAGS.task),
-        num_clients_per_round=FLAGS.clients_per_round,
-        num_rounds=FLAGS.total_rounds,
-        client_template=model_trainable_variables)
+  # Most logic for deciding what to run is here.
+  aggregation_factory = fl_utils.build_aggregator(
+      compression_flags=compression_dict,
+      dp_flags=dp_dict,
+      num_clients=NUM_TRAIN_CLIENTS,
+      num_clients_per_round=FLAGS.clients_per_round,
+      num_rounds=FLAGS.total_rounds,
+      client_template=model_trainable_variables)
 
-    return tff.learning.build_federated_averaging_process(
-        model_fn=model_fn,
-        server_optimizer_fn=server_optimizer_fn,
-        client_weighting=tff.learning.ClientWeighting.UNIFORM,
-        client_optimizer_fn=client_optimizer_fn,
-        model_update_aggregation_factory=aggregation_factory)
+  iterative_process = tff.learning.build_federated_averaging_process(
+      model_fn=task.model_fn,
+      server_optimizer_fn=server_optimizer_fn,
+      client_weighting=tff.learning.ClientWeighting.UNIFORM,
+      client_optimizer_fn=client_optimizer_fn,
+      model_update_aggregation_factory=aggregation_factory)
+  train_data = task.datasets.train_data.preprocess(
+      task.datasets.train_preprocess_fn)
+  training_process = (
+      tff.simulation.compose_dataset_computation_with_iterative_process(
+          train_data.dataset_computation, iterative_process))
 
-  task_spec = training_specs.TaskSpec(
-      iterative_process_builder=iterative_process_builder,
-      client_epochs_per_round=FLAGS.client_epochs_per_round,
-      client_batch_size=FLAGS.client_batch_size,
-      clients_per_round=FLAGS.clients_per_round,
-      client_datasets_random_seed=FLAGS.client_datasets_random_seed)
+  client_selection_fn = functools.partial(
+      tff.simulation.build_uniform_sampling_fn(
+          train_data.client_ids, random_seed=FLAGS.client_datasets_random_seed),
+      size=FLAGS.clients_per_round)
+  validation_fn = training_utils.create_validation_fn(
+      task,
+      validation_frequency=FLAGS.rounds_per_eval,
+      num_validation_examples=NUM_VALIDATION_EXAMPLES)
 
-  if FLAGS.task == 'stackoverflow_lr':
-    runner_spec = stackoverflow_tp.configure_training(task_spec)
-  else:
-    raise ValueError(
-        '--task flag {} is not supported, must be one of {}.'.format(
-            FLAGS.task, _SUPPORTED_TASKS))
+  def validation_fn_from_state(state, round_num):
+    return validation_fn(state.model, round_num)
 
-  def round_end_evaluation_fn(state, round_num):
-    if round_num % FLAGS.rounds_per_eval == 0:
-      validation_metrics = runner_spec.validation_fn(state, round_num)
-    else:
-      validation_metrics = {}
-    return validation_metrics
-
-  checkpoint_manager, metrics_managers = _configure_managers()
+  checkpoint_manager, metrics_managers = training_utils.configure_managers(
+      FLAGS.root_output_dir,
+      FLAGS.experiment_name,
+      rounds_per_checkpoint=FLAGS.rounds_per_checkpoint)
 
   state = tff.simulation.run_simulation(
-      process=runner_spec.iterative_process,
-      client_selection_fn=runner_spec.client_datasets_fn,
+      process=training_process,
+      client_selection_fn=client_selection_fn,
       total_rounds=FLAGS.total_rounds,
-      validation_fn=round_end_evaluation_fn,
+      validation_fn=validation_fn_from_state,
       file_checkpoint_manager=checkpoint_manager,
       metrics_managers=metrics_managers)
 
-  test_metrics = runner_spec.test_fn(state)
-
+  test_fn = training_utils.create_test_fn(task)
+  test_metrics = test_fn(state.model)
   logging.info('Test metrics:\n %s', pprint.pformat(test_metrics))
-
   for metrics_manager in metrics_managers:
     metrics_manager.save_metrics(test_metrics, FLAGS.total_rounds + 1)
 
